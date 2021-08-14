a96008ad051105a0b81dfe5012dd0d3f60c18265
PIAL <iftekherpial67@gmail.com>
2021-08-14 19:33:06 +0600

    First Commit Of Ecom

---
+++ app/Models/Products_attribute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products_attribute extends Model
+{
+    use HasFactory;
+}
